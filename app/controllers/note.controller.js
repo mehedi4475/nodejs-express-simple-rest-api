232ad1a9a642fac0adf812156c60d4c925756c06
Mehedi Hasan <mehedi4475@gmail.com>
2019-07-08 13:28:49 +0600

    Delete done

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -105,5 +105,24 @@ exports.update = (req, res) => {
 
 // Delete a note with the specified noteId in the request
 exports.delete = (req, res) => {
-
+	Note.findByIdAndRemove(req.params.noteId)
+	.then(note => {
+		if(!note){
+			return res.status(404).send({
+				message: "Note not found with id " + req.params.noteId
+			});
+		}
+		res.send({
+			message: "Note deleted successfully"
+		});
+	}).catch(err =>{
+		if(err.kind === 'ObjectId' || err.name === 'NotFound'){
+			return res.status(404).send({
+				message: 'Note not found with id ' + req.params.noteId
+			})
+		}
+		return res.status(500).send({
+			message: 'Could not delete with id ' + req.params.noteId
+		});
+	});
 };
