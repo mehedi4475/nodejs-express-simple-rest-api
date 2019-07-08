4ca5053fde8fa91350a162a55772471dd89cac1c
Mehedi Hasan <mehedi4475@gmail.com>
2019-07-08 13:02:57 +0600

    Find single note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -41,10 +41,31 @@ exports.findAll = (req, res) => {
 	})
 };
 
+
+
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
-	
+	Note.findById(req.params.noteId)
+	.then(note => {
+		if(!note){
+			return res.status(404).send({
+				message: "Note not found with id " + req.params.noteId
+			});
+		}
+		res.send(note);
+	}).catch(err => {
+		if(err.kind === 'ObjectId'){
+			return res.status(404).send({
+				message: "Note not found with id " + req.params.noteId
+			});
+		}
+		return res.status(500).send({
+			message: "Error retrieving note with id " + req.params.noteId
+		});
+	});	
 };
+
+
 
 // Update a note identified by the noteId in the request
 exports.update = (req, res) => {
